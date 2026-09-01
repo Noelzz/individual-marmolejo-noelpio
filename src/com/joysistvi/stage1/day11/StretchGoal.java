892910965338888892de68e8b860cec3847571da
Noel <noelpiomarmolejo@gmail.com>
2026-09-02 00:30:46 +0800

    Activity 9 : String Class

--- src/com/joysistvi/stage1/day11/StretchGoal.java
+++ src/com/joysistvi/stage1/day11/StretchGoal.java
@@ -1,30 +1,25 @@
 package com.joysistvi.stage1.day11;
 
 public class StretchGoal {
+
     public static void main(String[] args) {
 
         String country = "Philippines";
 
-        // Display the first and last character
         System.out.println("Country: " + country);
         System.out.println("First character: " + country.charAt(0));
         System.out.println("Last character: " + country.charAt(country.length() - 1));
 
-        // Count P/p and vowels
         int pCount = 0;
         int vowelCount = 0;
 
-        for (char character : country.toCharArray()) {
+        for (char c : country.toLowerCase().toCharArray()) {
 
-            // Count P/p
-            if (character == 'P' || character == 'p') {
+            if (c == 'p') {
                 pCount++;
             }
 
-            // Count vowels
-            if (character == 'a' || character == 'e' ||
-                    character == 'i' || character == 'o' ||
-                    character == 'u') {
+            if ("aeiou".indexOf(c) != -1) {
                 vowelCount++;
             }
         }
@@ -33,4 +28,6 @@ public class StretchGoal {
         System.out.println("Number of vowels: " + vowelCount);
     }
 }
+
+
 
